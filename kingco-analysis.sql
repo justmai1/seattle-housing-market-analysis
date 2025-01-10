a4652beecdf45936e4e27936f1873a3a0608a2cb
Justin Mai <justmai@uw.edu>
2025-01-10 11:44:02 -0800

    Update analysis files

--- kingco-analysis.sql
+++ kingco-analysis.sql
@@ -3,7 +3,6 @@
 SELECT DATEPART(year, sale_date) as year, city, ROUND(AVG(sale_price),2) as avg_sale_price
 FROM kingco_sales
 WHERE DATEPART(year, sale_date) >= 2000
-AND (city = 'SEATTLE' OR city = 'BELLEVUE' OR city = 'KING COUNTY')
 GROUP BY DATEPART(year, sale_date), city
 ORDER BY city, year;
 
@@ -44,4 +43,57 @@ WITH ppsqft as(
 SELECT year, city, AVG(ppsqft) as price_per_sqft
 FROM ppsqft
 GROUP BY city, year
-ORDER BY city, year
+ORDER BY city, year;
+
+-- Average annual income growth rate (percentage) vs Average rate of growth for home prices (percentage)
+WITH initial as(
+    SELECT DATEPART(year,observation_date) as year, annual_income as income
+    FROM seattle_aai
+    WHERE DATEPART(year,observation_date) >= 1999
+), rates as(
+    SELECT year + 1 as year, (((LEAD(income) OVER (ORDER BY year)) / income) - 1)*100 as rate
+    FROM initial
+)
+SELECT ROUND(AVG(rate),2) as avg_rate
+FROM rates;
+
+WITH initial as(
+    SELECT DATEPART(year,observation_date) as year, annual_income as income
+    FROM seattle_aai
+    WHERE DATEPART(year,observation_date) >= 2010
+), rates as(
+    SELECT year + 1 as year, (((LEAD(income) OVER (ORDER BY year)) / income) - 1)*100 as rate
+    FROM initial
+)
+SELECT ROUND(AVG(rate),2) as avg_rate
+FROM rates;
+
+WITH average_sale_price as(
+    SELECT DATEPART(year, sale_date) as year, city, ROUND(AVG(sale_price),2) as avg_sale_price
+    FROM kingco_sales
+    WHERE DATEPART(year, sale_date) >= 1999
+    GROUP BY DATEPART(year, sale_date), city
+), rate as(
+    SELECT year + 1 as year, 
+        city, 
+        (((LEAD(avg_sale_price) OVER (PARTITION BY city ORDER BY year) / avg_sale_price) - 1) * 100) as rate
+    FROM average_sale_price
+)
+SELECT city, ROUND(AVG(rate),2) avg_rate
+FROM rate
+GROUP BY city;
+
+WITH average_sale_price as(
+    SELECT DATEPART(year, sale_date) as year, city, ROUND(AVG(sale_price),2) as avg_sale_price
+    FROM kingco_sales
+    WHERE DATEPART(year, sale_date) >= 2010
+    GROUP BY DATEPART(year, sale_date), city
+), rate as(
+    SELECT year + 1 as year, 
+        city, 
+        (((LEAD(avg_sale_price) OVER (PARTITION BY city ORDER BY year) / avg_sale_price) - 1) * 100) as rate
+    FROM average_sale_price
+)
+SELECT city, ROUND(AVG(rate),2) avg_rate
+FROM rate
+GROUP BY city;
